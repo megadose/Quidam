065a016abf8d14dd33362c9c73e685b4f0f82160
megadose <51822495+megadose@users.noreply.github.com>
2020-05-28 17:14:48 +0200

    Update quidam.py

--- quidam.py
+++ quidam.py
@@ -1,88 +1,19 @@
-#The Content has been made available for informational and educational purposes only
-import argparse
-from quidam import *
-
-emailsFilesName="emails.txt"
-listModules = ["instagram","twitter","github"]
-
-parser = argparse.ArgumentParser()
-requiered = parser.add_argument_group('requiered arguments')
-parser.add_argument('-u', '--username', help="The uername of the target",required=True)
-parser.add_argument('-m', '--module', help="Modules to use instagram, twitter, github or all",required=True)
-args = parser.parse_args()
-
-#print("Quidam get email with recovery password")
-print("You select "+args.module)
-
-def possibleDomain(emailInfo):
-    EmailsFile = open(emailsFilesName, 'r')
-    Emails = EmailsFile.readlines()
-    EmailsFile.close()
-    print("Possible email : ")
-    emailProvider = emailInfo.split("@")[1]
-    for email in Emails:
-        email=email.replace("\n","")
-        if emailProvider[0]==email[0]:
-            if len(emailProvider.split(".")[0])==len(email.split(".")[0]):
-                print(emailInfo.split("@")[0]+"@"+email)
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
 
-if args.module=="instagram":
-    info = instagram(args.username)
-    if '"status"' not in info:
-        if 'This IP are blocked from instagram' not in info:
-            print("Email extract with instagram of "+args.username+": "+info)
-        else:
-            print(info)
-    else:
-        print(args.username+" account not found in instagram")
-
-elif args.module=="twitter":
-    info = twitter(args.username)
-    if len(info)==2:
-        print("The end of the phone number in twitter of "+args.username+": "+str(info["phone"]))
-        print("Email extract with twitter of "+args.username+": "+info["email"])
-        possibleDomain(info["email"])
-    elif len(info)==1 :
-        print("Email extract with twitter of "+args.username+": "+info["email"])
-        possibleDomain(info["email"])
-    else:
-        print("Not informations found in twitter")
-
-elif args.module=="github":
-    info = github(args.username)
-    if len(info)==0:
-        print("Not informations found in github")
-    else:
-        print("All emails found in github :")
-        for e in info:
-            print("Email "+e["email"]+" for "+e["name"])
-
-elif args.module=="all":
-    info = instagram(args.username)
-    if '"status"' not in info:
-        print("Email extract with instagram of "+args.username+": "+info)
-    else:
-        print(args.username+" account not found")
-    info = twitter(args.username)
-    if len(info)==2:
-        print("The end of the phone number in twitter of "+args.username+": "+str(info["phone"]))
-        print("Email extract with twitter of "+args.username+": "+info["email"])
-        possibleDomain(info["email"])
-    elif len(info)==1 :
-        print("Email extract with twitter of "+args.username+": "+info["email"])
-        possibleDomain(info["email"])
-    else:
-        print("Not informations found")
-    info = github(args.username)
-    if len(info)==0:
-        print("Not informations found in github")
-    else:
-        print("All emails found in github :")
-        for e in info:
-            print("Email "+e["email"]+" for "+e["name"])
-
-else:
-    print("List of modules :")
-    for i in listModules:
-        print(i)
+setup(
+    name='quidam',
+    version="1.23",
+    packages=find_packages(),
+    author="megadose",
+    install_requires=["requests","fake_useragent","evolut","argparse"],
+    description="Permet de recupérer des informations grace a la fonction mot de passe oubliée de certains sites",
+    long_description="",
+    include_package_data=True,
+    url='http://github.com/megadose/Quidam',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
